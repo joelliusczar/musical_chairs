b0b67e1e4ffa21c6be9db1e65b6808a018a56458
Joel Gillette <joelliuscaesar@gmail.com>
2021-04-04 01:05:46 -0400

    added an init

--- commit-setup.sh
+++ commit-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-lib_path='/home/ubuntu/.local/lib/python2.7/site-packages/musical_chairs_gen'
+lib_path='/home/ubuntu/.local/lib/python2.7/site-packages/musical_chairs_libs'
 
 if [ -e "lib_path"]; then
     rm -rf "$lib_path/*"
@@ -8,5 +8,5 @@ else
     mkdir -pv "$lib_path" 
 fi
 
-cp -rv ./src/common/ "$lib_path/"
+cp -rv ./src/common/ "$lib_path"
 

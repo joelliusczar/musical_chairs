362d35b022055509bb53bc759b4ff5003b25e8e0
Joel Gillette <joelliuscaesar@gmail.com>
2021-04-04 01:16:50 -0400

    adjusting my rm

--- commit-setup.sh
+++ commit-setup.sh
@@ -3,7 +3,7 @@
 lib_path='/home/ubuntu/.local/lib/python2.7/site-packages/musical_chairs_libs'
 
 if [ -e "lib_path"]; then
-    rm -rf "$lib_path/*"
+    rm -rf "$lib_path/"*
 else
     mkdir -pv "$lib_path" 
 fi

a01b806e46267f2e2c6c1f95690c99b688c448a6
Joel Gillette <joelliuscaesar@gmail.com>
2021-05-09 01:56:17 -0400

    forgot a copy line in setup script

--- commit-setup.sh
+++ commit-setup.sh
@@ -47,12 +47,19 @@ else
     mkdir -pv "$app_path" 
 fi
 
+#set up the python environment, then copy 
 cd ./src/api
 virtualenv env &&
 ./env/bin/pip3 install -r ./requirements.txt &&
 cd - &&
+cp -r ./src/common/* ./env/lib/python3.8/site-packages/ &&
 cp -rv ./src/api "$app_path/"
 
+#set up react then copy
 npm run --prefix ./src/client build &&
 mkdir -pv "$app_path"/client &&
 cp -rv ./src/client/build "$app_path"/client/
+
+
+# proj_env_py3="$(dirname "$0")""$api_env_path"/lib/python3.8/site-packages
+# usePath_proj="$proj_env_py3"/"$libName"

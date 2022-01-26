5aabc2abee752611889fa3e232e71ef7427f43c7
joelliusczar <joelliuscaesar@gmail.com>
2022-01-26 10:45:37 -0500

    missing path

--- icecast_check.sh
+++ icecast_check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 test_flag="$1"
-[ "$test_flag" = "test" ] && defs_home='../' || defs_home="$HOME"
+[ "$test_flag" = "test" ] && defs_home='../' || defs_home="$HOME"/radio
 
 . "$defs_home"/radio_common.sh
 

cb5462e0905d7188d7a06482c718157e4a4a8f67
Joel Gillette <joelliuscaesar@gmail.com>
2022-03-11 20:46:28 -0500

    fixed a space issue with calling my radio_common script

--- icecast_check.sh
+++ icecast_check.sh
@@ -2,9 +2,9 @@
 
 if [ -e ./radio_common.sh ]; then
 . ./radio_common.sh
-elif [ -e ../radio_common.sh]; then
+elif [ -e ../radio_common.sh ]; then
 . ../radio_common.sh
-elif [ -e "$HOME"/radio/radio_common.sh]; then
+elif [ -e "$HOME"/radio/radio_common.sh ]; then
 . "$HOME"/radio/radio_common.sh
 else
 	echo "radio_common.sh not found"
--- install_setup.sh
+++ install_setup.sh
@@ -33,7 +33,8 @@ fi
 case $(uname) in
 	Darwin*)
 		if ! brew --version 2>/dev/null; then
-			/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+			/bin/bash -c "$(curl -fsSL \
+				https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 		fi
 		;;
 	*) ;;

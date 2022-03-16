9bef865bcf87335884d9bed876caec8bdf218e34
Joel Gillette <joelliuscaesar@gmail.com>
2022-03-15 20:49:37 -0400

    missing exhaustive radio common

--- radio_dir_setup.sh
+++ radio_dir_setup.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+if [ -e ./radio_common.sh ]; then
 . ./radio_common.sh
+elif [ -e ../radio_common.sh ]; then
+. ../radio_common.sh
+elif [ -e "$HOME"/radio/radio_common.sh ]; then
+. "$HOME"/radio/radio_common.sh
+else
+  echo "radio_common.sh not found"
+  exit 1
+fi
 
 cp ./templates/configs/config.yml "$config_file" &&
 sed -i -e "s@<searchbase>@$music_home/@" "$config_file" &&

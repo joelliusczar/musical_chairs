6bb7793c9abc8a2f80641b38a2a3c9947833941e
Joel Gillette <joelliuscaesar@gmail.com>
2022-04-27 08:53:15 -0400

    moved the stuff relevant only to compiling ices out of install_setup and to build_ices

--- compiled_dependencies/build_ices.sh
+++ compiled_dependencies/build_ices.sh
@@ -14,6 +14,69 @@ fi
 #we have this as a seperate script so I can easily run it without being tangled
 #with the rest
 
+if ! aclocal --version 2>/dev/null; then
+	install_package automake
+fi
+
+
+case $(uname) in
+	(Darwin*)
+		if ! brew_is_installed libtool; then
+			install_package libtool 
+		fi
+		if ! brew_is_installed pkg-config; then
+			install_package pkg-config
+		fi
+		if ! brew_is_installed libshout; then
+			install_package libshout 
+		fi
+		;;
+	(Linux*) 
+		if [ "$pkgMgrChoice" = "$APT_CONST" ] \
+		&& ! libtool --version 2>/dev/null; then
+			install_package libtool-bin
+		fi
+		;;
+	(*) ;;
+esac
+
+#leave this here incase the python check above did update the python version
+set_python_version_const
+
+if [ "$pkgMgrChoice" = "$APT_CONST" ]; then
+	if ! dpkg -s libxml2-dev >/dev/null 2>&1; then
+		install_package libxml2-dev
+	fi &&
+	if ! dpkg -s libogg-dev >/dev/null 2>&1; then
+		install_package libogg-dev
+	fi &&
+	if ! dpkg -s libvorbis-dev >/dev/null 2>&1; then
+		install_package libvorbis-dev
+	fi &&
+	if ! dpkg -s libshout3-dev >/dev/null 2>&1; then
+		install_package libshout3-dev
+	fi &&
+	if ! dpkg -s libmp3lame-dev >/dev/null 2>&1; then
+		install_package libmp3lame-dev
+	fi &&
+	if ! dpkg -s libflac-dev >/dev/null 2>&1; then
+		install_package libflac-dev
+	fi &&
+	if ! dpkg -s libfaad-dev >/dev/null 2>&1; then
+		install_package libfaad-dev
+	fi &&
+	if ! dpkg -s libperl-dev >/dev/null 2>&1; then
+		install_package libperl-dev
+	fi &&
+	if ! dpkg -s python3-distutils >/dev/null 2>&1; then
+		install_package python3-distutils
+	fi
+	if ! dpkg -s python"$pyMajor"."$pyMinor"-dev >/dev/null 2>&1; then
+		install_package python"$pyMajor"."$pyMinor"-dev
+	fi #continue even if python3.x-dev is not found bc if we compiled python,
+	#it may not actually exist
+fi
+
 case $(uname) in
 	Darwin*)
 		PATH="/opt/homebrew/opt/libtool/libexec/gnubin:$PATH"

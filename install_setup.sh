28ec8420837c590950ef0056e9aafa67cd4f0c7d
Joel Gillette <joelliuscaesar@gmail.com>
2022-05-04 22:26:33 -0400

    just downloading the get-pip script

--- install_setup.sh
+++ install_setup.sh
@@ -77,7 +77,9 @@ fi || show_err_and_exit "python install failed"
 mc-python -V 2>/dev/null || show_err_and_exit "mc-python not available"
 
 if ! mc-python -m pip -V 2>/dev/null; then
-	mc-python -c "$(curl -fsSL https://bootstrap.pypa.io/pip/get-pip.py)" ||
+	curl -o "$app_root"/"$build_dir"/get-pip.py \
+		https://bootstrap.pypa.io/pip/get-pip.py &&
+	mc-python "$app_root"/"$build_dir"/get-pip.py ||
 	show_err_and_exit "Couldn't install pip"
 fi
 

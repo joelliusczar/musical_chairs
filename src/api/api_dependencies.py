9b292e1ef86fd5ee23cd9354c0cdfda98f1976a6
Joel Gillette <joelliuscaesar@gmail.com>
2022-05-05 10:31:12 -0400

    fixed a bad import in api

--- src/api/api_dependencies.py
+++ src/api/api_dependencies.py
@@ -1,6 +1,6 @@
 from typing import Iterator
 from fastapi import Depends
-from musical_chairs_libs.wrapped_db_connection import WrappedDbConnection
+from sqlalchemy.engine import Connection
 from musical_chairs_libs.env_manager import EnvManager
 from musical_chairs_libs.history_service import HistoryService
 from musical_chairs_libs.station_service import StationService
@@ -8,7 +8,7 @@ from musical_chairs_libs.queue_service import QueueService
 
 def get_configured_db_connection(
 	envManager: EnvManager = Depends(EnvManager)
-) -> Iterator[WrappedDbConnection]:
+) -> Iterator[Connection]:
 	if not envManager:
 		envManager = EnvManager()
 	conn = envManager.get_configured_db_connection()
@@ -18,16 +18,16 @@ def get_configured_db_connection(
 		conn.close()
 
 def station_service(
-	conn: WrappedDbConnection = Depends(get_configured_db_connection)
+	conn: Connection = Depends(get_configured_db_connection)
 ) -> StationService:
 	return StationService(conn)
 
 def history_service(
-	conn: WrappedDbConnection = Depends(get_configured_db_connection)
+	conn: Connection = Depends(get_configured_db_connection)
 ) -> HistoryService:
 	return HistoryService(conn)
 
 def queue_service(
-	conn: WrappedDbConnection = Depends(get_configured_db_connection)
+	conn: Connection = Depends(get_configured_db_connection)
 ) -> QueueService:
 	return QueueService(conn)

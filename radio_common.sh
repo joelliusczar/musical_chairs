8a925f2774cd1ab811a9220dc73903b8d3c53cad
Joel Gillette <joelliuscaesar@gmail.com>
2022-05-08 22:34:35 -0400

    redirecting stdin for my api startup so that it's not keeping ssh open

--- radio_common.sh
+++ radio_common.sh
@@ -766,10 +766,8 @@ startup_api() (
 	export dbName="$app_root"/"$sqlite_file" &&
 	. "$web_root"/"$app_api_path_cl"/"$py_env"/bin/activate &&
 	# see #python_env
-	(
-		uvicorn --app-dir "$web_root"/"$app_api_path_cl" \
-		--host 0.0.0.0 --port "$api_port" "index:app" 
-	) >api.out 2>&1 &
+	uvicorn --app-dir "$web_root"/"$app_api_path_cl" \
+	--host 0.0.0.0 --port "$api_port" "index:app" </dev/null >api.out 2>&1 &
 	echo "Done with api"
 )
 

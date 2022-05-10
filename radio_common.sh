a2721a42eb375caf0a30a8cddc252fa4909f90dd
Joel Gillette <joelliuscaesar@gmail.com>
2022-05-10 17:49:11 -0400

    print statement

--- radio_common.sh
+++ radio_common.sh
@@ -705,7 +705,7 @@ create_new_station() (
 	
 	create_ices_config "$internalName" "$publicName" "$sourcePassword" &&
 	create_ices_py_module "$internalName" &&
-	echo ""
+	echo "skip_option: ${skip_option}"
 	if [ "$skip_option" = 'save_station' ]; then
 		echo "Not saving to db"
 		return

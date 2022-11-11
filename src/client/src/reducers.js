5afc868c07cfaa73664d8bc0ab9993873020f45a
Joel Gillette <joelliuscaesar@gmail.com>
2022-11-11 08:54:35 -0500

    strip away more redux stuff

--- src/client/src/reducers.js
+++ src/client/src/reducers.js
@@ -1,16 +1,11 @@
-import queueReducer from "./Components/Queue/queue_slice";
 import historyReducer from "./Components/History/history_slice";
-import songCatalogueReducer
-	from "./Components/Song_Catalogue/songInfoService";
 import accountsReducer from "./API_Calls/userCalls";
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 
 
 const reducer = {
-	queue: queueReducer,
 	history: historyReducer,
-	songCatalogue: songCatalogueReducer,
 	accounts: accountsReducer,
 };
 

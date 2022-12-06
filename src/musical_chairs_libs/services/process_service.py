5cd279e71ff41795d8ab4029d8c9f5e7117dae72
Joel Gillette <joelliuscaesar@gmail.com>
2022-12-05 22:17:42 -0500

    fixed config path error

--- src/musical_chairs_libs/services/process_service.py
+++ src/musical_chairs_libs/services/process_service.py
@@ -137,7 +137,7 @@ class ProcessService:
 		m = check_name_safety(stationName)
 		if m:
 			raise RuntimeError("Invalid station name was used")
-		stationConf = f"{EnvManager.station_config_dir}/{stationName}.conf"
+		stationConf = f"{EnvManager.station_config_dir}/ices.{stationName}.conf"
 		if not os.path.isfile(stationConf):
 			raise LookupError(f"Station not found at: {stationConf}")
 		if platform.system() == "Darwin":

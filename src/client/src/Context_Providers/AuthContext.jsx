be81ded396c44408d42ce7bd2923ad20fba097f0
Joel Pridgen <joelliuscaesar@gmail.com>
2023-01-28 06:32:07 -0500

    fixed cookie expiration, I guess. And fixed issue of click away on user menu not working (#37)

--- src/client/src/Context_Providers/AuthContext.jsx
+++ src/client/src/Context_Providers/AuthContext.jsx
@@ -31,7 +31,7 @@ const loggedOutState = {
 };
 
 const expireCookie = (name) => {
-	document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+	document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/;";
 };
 
 
